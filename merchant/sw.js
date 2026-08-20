67dfa0d155821c85916e9853b32c33e66824379e
Future Cosmetics <waleedgamaln@gmail.com>
2026-08-20 11:15:00 +0300

    تحديث

---
+++ merchant/sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'deliveroo-merchant-v1';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
